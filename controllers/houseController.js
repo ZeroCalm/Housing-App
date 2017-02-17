38fa8535772b76c640d4d616a59635bf16eb08e9
zeroCalm <smith.timothyh@gmail.com>
2017-02-17 15:25:56 -0800

    get one returns json

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -6,17 +6,28 @@
 //
 var db = require('../models');
 
-// GET /api/albums
+// GET /api/houses
 function index(req, res) {
-  // send back all albums as JSON
+  // send back all houses as JSON
   db.House.find({}, function(err, allHouses) {
     res.json(allHouses);
   });
 }
 
-// POST /api/albums
+// GET /api/houses/:houseId
+function show(req, res) {
+  // find one house by id and send it back as JSON
+  db.House.findById(req.params.houseId, function(err, foundHouse) {
+    if(err) { console.log('houseController.show error', err); }
+    console.log('houseController.show responding with', foundHouse);
+    res.json(foundHouse);
+  });
+}
+
+
+// POST /api/houses
 function create(req, res) {
-  // create an album based on request body and send it back as JSON
+  // create an house based on request body and send it back as JSON
   console.log('body', req.body);
   db.House.create(req.body, function(err, house) {
     if (err) { console.log('error', err); }
@@ -26,40 +37,31 @@ function create(req, res) {
 }
 
 
-// DELETE /api/albums/:albumId
+// DELETE /api/houses/:houseId
 function destroy(req, res) {
-  // find one album by id, delete it, and send it back as JSON
+  // find one house by id, delete it, and send it back as JSON
   db.House.findOneAndRemove({ _id: req.params.houseId }, function(err, foundHouse){
     // note you could send just send 204, but we're sending 200 and the deleted entity
     res.json(foundHouse);
   });
 }
 //
-// // GET /api/albums/:albumId
-// function show(req, res) {
-//   // find one album by id and send it back as JSON
-//   db.Album.findById(req.params.albumId, function(err, foundAlbum) {
-//     if(err) { console.log('albumsController.show error', err); }
-//     console.log('albumsController.show responding with', foundAlbum);
-//     res.json(foundAlbum);
-//   });
-// }
-//
+
 
 //
-// // PUT or PATCH /api/albums/:albumId
+// // PUT or PATCH /api/houses/:houseId
 // function update(req, res) {
-//   // find one album by id, update it based on request body,
+//   // find one house by id, update it based on request body,
 //   // and send it back as JSON
 //   console.log('updating with data', req.body);
-//   db.Album.findById(req.params.albumId, function(err, foundAlbum) {
-//     if(err) { console.log('albumsController.update error', err); }
-//     foundAlbum.artistName = req.body.artistName;
-//     foundAlbum.name = req.body.name;
-//     foundAlbum.releaseDate = req.body.releaseDate;
-//     foundAlbum.save(function(err, savedAlbum) {
-//       if(err) { console.log('saving altered album failed'); }
-//       res.json(savedAlbum);
+//   db.house.findById(req.params.houseId, function(err, foundhouse) {
+//     if(err) { console.log('housesController.update error', err); }
+//     foundhouse.artistName = req.body.artistName;
+//     foundhouse.name = req.body.name;
+//     foundhouse.releaseDate = req.body.releaseDate;
+//     foundhouse.save(function(err, savedhouse) {
+//       if(err) { console.log('saving altered house failed'); }
+//       res.json(savedhouse);
 //     });
 //   });
 // }
@@ -68,6 +70,6 @@ function destroy(req, res) {
 module.exports = {
   index: index,
   create: create,
-  destroy: destroy
-
+  destroy: destroy,
+  show: show
 };
