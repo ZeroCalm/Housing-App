611eeae6aff5b470c9b2577e44667021e86e781f
zeroCalm <smith.timothyh@gmail.com>
2017-02-16 22:34:37 -0800

    db has been seeded

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 console.log("Hello World");
+=======
+$(document).ready(function() {
+  console.log('app.js loaded!');
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/listings',
+    success: renderMultipleListings
+  });
+})
+>>>>>>> 5c9875dccca793fb3205dfdb52e8500f5eea823d
