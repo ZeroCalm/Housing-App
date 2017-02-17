2799a11cac44b2a86a559a222cf9f9f1ab71477b
monzelb <monzelb@gmail.com>
2017-02-17 10:27:48 -0800

    fixed whitespace errors

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
 
+  $.ajax({
+     method: 'GET',
+     url: '/api/listings',
+     success: renderMultipleListings
+   });
+ }) 
 
 var houseList =[];
 houseList.push({
