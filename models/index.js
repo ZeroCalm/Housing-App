d13d3bb6b44dadbd06d00da73049bc03a34023fc
zeroCalm <smith.timothyh@gmail.com>
2017-02-17 16:10:38 -0800

    update is returning json

--- models/index.js
+++ models/index.js
@@ -3,6 +3,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/housing");
 var House = require('./house');
 
 module.exports.House = House;
-
-//WIll add line below after citySchema is created.
-//module.exports.City = require('./city');
